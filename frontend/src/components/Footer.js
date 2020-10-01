b689209851355ae5c96ce966c6d1d0f9fa88e5fd
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-01 02:28:49 -0500

    Navbar setting + footer creation

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return <footer>
+      <Row>
+          <Col className="text-center my-3">Copyright &copy; My Shop</Col>
+      </Row>
+  </footer>;
+};
+
+export default Footer;
