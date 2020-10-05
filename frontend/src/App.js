4271bc8d4a243d46d3c1e29c058937b9586bdf7e
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-05 01:12:40 -0500

    Product screen detail finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,23 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./Screens/HomeScreen";
+import { BrowserRouter, Route } from 'react-router-dom';
+import ProductScreen from "./Screens/ProductScreen";
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className="my-3">
         <Container>
-          <h1>Welcome to My Shop</h1>
+          <Route path="/" exact component={HomeScreen}/>
+          <Route path="/product/:id" exact component={ProductScreen}/>
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Screens/ProductScreen.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Button, ListGroup, Image, Card } from "react-bootstrap";
+import products from "../products";
+import Rating from "../components/Rating";
+
+const ProductScreen = (props) => {
+  const product = products.find((item) => item._id === props.match.params.id);
+
+  return (
+    <>
+      <Link to="/" className="btn btn-light my-3">
+        Go back
+      </Link>
+      <Row>
+        <Col xl={6}>
+          <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+        <Col xl={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>
+               Price: ${product.price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+               Description: {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col xl={3} className="mt-sm-4 mt-xl-0">
+            <Card>
+                <ListGroup>
+                    <ListGroup.Item>
+                        <Row>
+                            <Col xs={6}>Price:</Col>
+                            <Col xs={6}><strong>${product.price}</strong></Col>
+                        </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Row>
+                            <Col xs={6}>In Stock:</Col>
+                            <Col xs={6}>{product.countInStock ? "In stock" : "Out of stock"}</Col>
+                        </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                       <Button className="btn btn-block" type="button"
+                       disabled={product.countInStock === 0}>add to cart</Button>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
