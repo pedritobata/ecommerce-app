e6426221f848cf766dc374c03ec116f5c0a0c157
unknown <pedromartinez_goyo@hotmail.com>
2020-11-04 15:44:24 -0500

    backend pending functionalities implemented

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, getProductsById } = require('../controllers/productControllers');
+const {   getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,} = require('../controllers/productControllers');
+const { protect, admin } = require('../middleware/authMiddleware');
 
-
-router.route("/").get(getProducts);
-
-router.route("/:id").get(getProductsById);
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,60 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const path = require('path'); 
+const express = require('express'); 
+const dotenv = require('dotenv'); 
+const colors = require('colors'); 
+const morgan = require('morgan'); 
+const { notFound, errorHandler } = require('./middleware/errorMiddleware.js'); 
+const connectDB = require('./config/db.js'); 
+
+const productRoutes = require('./routes/productRoutes.js'); 
+const userRoutes = require('./routes/userRoutes.js'); 
+const orderRoutes = require('./routes/orderRoutes.js'); 
+const uploadRoutes = require('./routes/uploadRoutes.js'); 
 
 dotenv.config();
 
-
-const productRoutes = require('./routes/productRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-const port = process.env.PORT || 5000;
-const app = express();
-const connectDB = require('./config/db');
-
 connectDB();
 
-app.use(cors());
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
-app.get("/", (req,res) => {
-    res.send("Ecommerce Backend running yeah...");
-});
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
+const dirname = path.resolve();
+app.use('/uploads', express.static(path.join(dirname, '/uploads')));
 
-app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} environment on port ${port}`))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  })
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
